d48d9d90a8aee763b5852946986911a5219233be
Marine11234 <kanDAhai1234566@163.com>
2024-02-11 12:03:04 +0800

    user change 1

--- src/user/first.java
+++ src/user/first.java
@@ -2,6 +2,7 @@ package user;
 
 public class first {
     public static void main(String[] args) {
-        System.out.println("Hello would");
+        System.out.println("Hello would.");
+        System.out.println("My name is ocoan.");
     }
 }
